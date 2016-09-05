f5e0d15d7f0e4d8cb1a361f7be68b97f06cc26d4
heronlyj <heronlyj@gmail.com>
2016-09-05 13:41:34 +0800

    add missing files

--- CapturePopAction/PopControlViewController.swift
+++ CapturePopAction/PopControlViewController.swift
@@ -12,24 +12,36 @@ class PopControlViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension PopControlViewController: PopControlAble {
+    
+    func shouldPop(controller controller: UINavigationController) -> Bool {
+        
+        let shouldPopAction = UIAlertAction(title: "泥奏凯, 别拦我", style: .Default) { (action: UIAlertAction) in
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+        
+        let cancel = UIAlertAction(title: "我再想想..", style: .Cancel, handler: nil)
+        
+        let alertController = UIAlertController(title: "真的要走吗.. /(ㄒoㄒ)/~~", message: nil, preferredStyle: .Alert)
+        
+        alertController.addAction(shouldPopAction)
+        alertController.addAction(cancel)
+        
+        presentViewController(alertController, animated: true) {
+            // 将 backButton 的颜色恢复原状
+            self.navigationItem.setHidesBackButton(true, animated: true)
+            self.navigationItem.setHidesBackButton(false, animated: true)
+        }
+        
+        return false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
